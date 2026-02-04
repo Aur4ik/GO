7007588ecfd73cde01238fb4108870b9b429cddc
Aurik <Aurikgg@gmail.com>
2026-02-04 16:27:42 +0500

    Third Control

---
+++ third/main.go
@@ -0,0 +1,104 @@
+package main
+
+import "fmt"
+
+// func printInfo(s []int) {
+// 	fmt.Println(s, len(s), cap(s))
+// }
+// func setZeroFirst(s []int){
+// 	s[0] = 0
+// 	fmt.Println("In function:", s)
+// }
+// func addNum(s []int, value int)[]int{
+// 	s = append(s, value)
+// 	return s
+// }
+// func create(s []int, value int)[]int{
+// 	s = append(s, value)
+// 	return s
+// }
+// func edit(s []int) []int{
+// 	s[0] = 777
+// 	return s
+// }
+
+// func editnum(s []int) []int{
+// 	s[0] = 100
+// 	s[1] = 200
+// 	s[2] = 300
+// 	return s
+// }
+
+//Короче тут чуть чуть пиздец потому что неекоторые задания 
+//требуют те же функции что я писал и условно так же вывести массивы
+//Плюс я задания делал потом опускал тх вниз чтоб сразу можно было написать функцию
+//и типо они читаются снизу вверх.Короче поверьте на слово что я все сделал
+// Бля буду отвечаю
+
+
+func main() {
+//Доп задания 
+// nums := []int{1,2,3,4,5,6,7,8,9,10}
+
+// slice := nums[:4]
+// slice2 := nums[4:]
+
+
+// fmt.Println(slice)
+// fmt.Println(slice2)
+
+// slice = append(slice[:2],slice[3:]...)
+
+// fmt.Println(slice)
+
+// slice := nums[:2]
+// slice2 := nums[3:6]
+
+// slice3 := append(slice, slice2...)
+
+// fmt.Println(slice3)
+
+
+
+// fmt.Println("До расширения:")
+// fmt.Println("len =", len(slice))
+// fmt.Println("cap =", cap(slice))
+
+
+// slice = slice[:cap(slice)]
+
+
+// fmt.Println("\nПосле расширения:")
+// fmt.Println("len =", len(slice))
+// fmt.Println("cap =", cap(slice))
+
+// fmt.Println("Срез:", slice)
+
+
+// slice = editnum(slice)
+// fmt.Println(nums)
+// fmt.Println(slice)
+//Классная работа
+// var s []int
+
+// s = addNum(s, 10)
+
+
+// arr := make([]int, 0, 2)
+
+// arr = addNum(arr, 10)
+// arr = addNum(arr, 20)
+// arr = addNum(arr, 40)
+
+// nums := []int{1,2,3,4,5}
+// slice := nums[1:4]
+
+// nums = edit(slice)
+
+// nums = create(nums, 10)
+// printInfo(nums)
+// setZeroFirst(nums)
+// nums = addNum(nums, 200)
+
+// fmt.Println(arr,len(arr), cap(arr))
+}
